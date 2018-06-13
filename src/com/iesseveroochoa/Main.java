f3e413d616b36fa7ef2e76ddb574c73cd6b78d5f
joel <joelnoche@gmail.com>
2018-06-13 12:05:53 +0200

    Funciona OK

--- src/com/iesseveroochoa/Main.java
+++ src/com/iesseveroochoa/Main.java
@@ -2,12 +2,13 @@ package com.iesseveroochoa;
 
 import jdk.nashorn.internal.objects.NativeArray;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    List<Personaje> reparto = null;
+    static List<Personaje> reparto = new ArrayList<Personaje>();
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         //Incorporación de los personajes a la obra.
         incorporarAlReparto();
         //Desarrollo de la trama.
@@ -20,7 +21,7 @@ public class Main {
 
     }
 
-    void incorporarAlReparto() {
+    static void incorporarAlReparto() {
         reparto.add(new Personaje("Odiseo", "héroe"));
         reparto.add(new Personaje("Penelepe", "esposa"));
         reparto.add(new Personaje("Telémaco", "heredero"));
@@ -30,7 +31,7 @@ public class Main {
         reparto.add(new Personaje("Nestor","rey"));
     }
 
-    void ubicaciónIncial(){
+    static void ubicaciónIncial(){
         for (int i = 0; i < reparto.size(); i++) {
             if (reparto.get(i).getNombre().equals("Odiseo")) {
                 reparto.get(i).setLocalizacion("Troya"); //Odiseo está en Troya.
@@ -40,7 +41,7 @@ public class Main {
         }
     }
 
-    void viajeros() {
+    static void viajeros() {
         for (int i = 0; i < reparto.size(); i++) {
             System.out.print("El personaje " + reparto.get(i).getNombre());
             System.out.println(" viaja hasta " + reparto.get(i).getLocalizacion());
